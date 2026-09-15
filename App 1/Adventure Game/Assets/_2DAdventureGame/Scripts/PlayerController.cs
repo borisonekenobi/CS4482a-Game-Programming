0c94c6dcdfbc482a596e81fed90b9329700988db
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-15 16:41:12 -0400

    Test the default 2D Beginner movement

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
