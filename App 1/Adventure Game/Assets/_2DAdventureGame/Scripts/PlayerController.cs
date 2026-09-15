e1f268c05da0bb4e0401d8d18cc04cf0cad5414e
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-15 16:21:39 -0400

    Define an Input Action

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public InputAction LeftAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
