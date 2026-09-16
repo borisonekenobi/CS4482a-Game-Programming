39e8eff3e966d56a8294c14ffecb6470ac7730d7
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-16 17:39:16 -0400

    Fix the protection level error

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     public int maxHealth = 5;
-    int currentHealth;
+    int currentHealth = 1;
     Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
+        // currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -32,7 +32,7 @@ public class PlayerController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
