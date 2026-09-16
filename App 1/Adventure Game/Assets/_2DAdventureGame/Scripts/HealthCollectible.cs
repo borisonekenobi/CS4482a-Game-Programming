b4dafb46e110a501e7efa70a85468c4a7ddef181
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-16 17:25:02 -0400

    Increase the player character health

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -5,5 +5,12 @@ public class HealthCollectible : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Object that entered the trigger: " + other);
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
     }
 }
