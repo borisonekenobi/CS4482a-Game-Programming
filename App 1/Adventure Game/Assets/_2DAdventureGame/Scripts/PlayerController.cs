c19cb9eb207e4577657db017ee6d8201da89bf30
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-16 10:11:07 -0400

    Adjust the movement to units per second

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -8,8 +8,8 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
@@ -18,7 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.01f;
+        Vector2 position = (Vector2)transform.position + move * 0.01f * Time.deltaTime;
         transform.position = position;
     }
 }
