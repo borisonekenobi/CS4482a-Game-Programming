8e7c69181848a37aa00dcf82c390f080fde8b2f4
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 11:11:28 -0400

    Optimize the projectiles

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/Projectile.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/Projectile.cs
@@ -14,7 +14,10 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.magnitude > 100.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Launch(Vector2 direction, float force)
