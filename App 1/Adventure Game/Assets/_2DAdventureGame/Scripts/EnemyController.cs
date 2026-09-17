73ad317f06a41e54815e9f2b69c6f4cc1fe84a48
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-16 23:24:20 -0400

    Add damage for enemy collisions

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/EnemyController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/EnemyController.cs
@@ -45,4 +45,14 @@ public class EnemyController : MonoBehaviour
 
         rigidbody2d.MovePosition(position);
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
 }
