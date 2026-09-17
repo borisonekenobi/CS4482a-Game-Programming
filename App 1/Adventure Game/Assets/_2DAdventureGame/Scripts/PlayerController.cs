8db4e549d053c8be2d0888a8a355c248a7cf563d
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 10:50:17 -0400

    Add a function to launch projectiles

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -23,6 +23,9 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     Vector2 moveDirection = new Vector2(1,0);
 
+    // Variables related to projectiles
+    public GameObject projectilePrefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -79,5 +82,13 @@ public class PlayerController : MonoBehaviour
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+        animator.SetTrigger("Launch");
     }
 }
