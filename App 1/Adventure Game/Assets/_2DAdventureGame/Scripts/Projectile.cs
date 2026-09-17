53f150c2adb8ed92397a1b92cdca22df5792d595
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 10:47:40 -0400

    Script the basic projectile functionality

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/Projectile.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/Projectile.cs
@@ -1,16 +1,30 @@
+using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    Rigidbody2D rigidbody2d;
+
+    // Awake is called when the Projectile GameObject is instantiated
+    void Awake()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Projectile collision with " + other.gameObject);
+        Destroy(gameObject);
     }
 }
