49feda8f90c869b12d375e9c3e92945cff81749d
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 00:24:03 -0400

    Send parameters to Animator Controller

--- App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/EnemyController.cs
+++ App 1/Adventure Game/Assets/_2DAdventureGame/Scripts/EnemyController.cs
@@ -7,6 +7,7 @@ public class EnemyController : MonoBehaviour
     public float changeTime = 3.0f;
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
     float timer;
     int direction = 1;
 
@@ -15,6 +16,7 @@ public class EnemyController : MonoBehaviour
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called every frame
@@ -37,10 +39,14 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + speed * direction * Time.deltaTime;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
         }
 
         rigidbody2d.MovePosition(position);
