2656cc69e2481259a2d4c41c679911622b800c0e
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 14:40:37 -0400

    Spawning a window

---
+++ Tools 1/Unity Editor Scripting/Assets/Editor/EnemyDesignerWindow.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyDesignerWindow : EditorWindow
+{
+    [MenuItem("Window/Enemy Designer")]
+    static void OpenWindow()
+    {
+        EnemyDesignerWindow window = (EnemyDesignerWindow)GetWindow(typeof(EnemyDesignerWindow));
+        window.minSize = new Vector2(600, 300);
+        window.Show();
+    }
+}
