08fd185ab36c3bd608f64748c181a27236e9ace5
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-17 15:12:20 -0400

    Add scriptable objects

---
+++ Tools 1/Unity Editor Scripting/Assets/Resources/CharacterData/Scripts/MageData.cs
@@ -0,0 +1,9 @@
+using Types;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Mage Data", menuName="Character Data/Mage")]
+public class MageData : CharacterData
+{
+    public MageDmgType dmgType;
+    public MageWpnType wpnType;
+}
---
+++ Tools 1/Unity Editor Scripting/Assets/Resources/CharacterData/Scripts/RogueData.cs
@@ -0,0 +1,9 @@
+using Types;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Rogue Data", menuName="Character Data/Rogue")]
+public class RogueData : CharacterData
+{
+    public RogueWpnType wpnType;
+    public RogueStrategyType strategyType;
+}
