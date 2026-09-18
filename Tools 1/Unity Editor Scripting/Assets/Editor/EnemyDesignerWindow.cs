b44cc24216f3e811c180af85e9aede34dd887222
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-18 11:50:06 -0400

    Buttons

--- Tools 1/Unity Editor Scripting/Assets/Editor/EnemyDesignerWindow.cs
+++ Tools 1/Unity Editor Scripting/Assets/Editor/EnemyDesignerWindow.cs
@@ -118,6 +118,11 @@ public class EnemyDesignerWindow : EditorWindow
         mageData.wpnType = (MageWpnType)EditorGUILayout.EnumPopup(mageData.wpnType);
         EditorGUILayout.EndHorizontal();
 
+        if (GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.MAGE);
+        }
+
         GUILayout.EndArea();
     }
 
@@ -136,6 +141,11 @@ public class EnemyDesignerWindow : EditorWindow
         GUILayout.Label("Weapon");
         warriorData.wpnType = (WarriorWpnType)EditorGUILayout.EnumPopup(warriorData.wpnType);
         EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.WARRIOR);
+        }
 
         GUILayout.EndArea();
     }
@@ -156,6 +166,31 @@ public class EnemyDesignerWindow : EditorWindow
         rogueData.wpnType = (RogueWpnType)EditorGUILayout.EnumPopup(rogueData.wpnType);
         EditorGUILayout.EndHorizontal();
 
+        if (GUILayout.Button("Create!", GUILayout.Height(40)))
+        {
+            GeneralSettings.OpenWindow(GeneralSettings.SettingsType.ROGUE);
+        }
+
         GUILayout.EndArea();
+    }
+}
+
+public class GeneralSettings : EditorWindow
+{
+    public enum SettingsType
+    {
+        MAGE,
+        WARRIOR,
+        ROGUE
+    }
+    static SettingsType dataSetting;
+    static GeneralSettings window;
+
+    public static void OpenWindow(SettingsType setting)
+    {
+        dataSetting = setting;
+        window = (GeneralSettings)GetWindow(typeof(GeneralSettings));
+        window.minSize = new Vector2(250, 200);
+        window.Show();
     }
 }
