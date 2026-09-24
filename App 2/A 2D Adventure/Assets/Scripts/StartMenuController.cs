aeaf0b70ddadb8591f7f96af09bdd55305d36ac5
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-24 13:40:57 -0400

    Replace UI assets

---
+++ App 2/A 2D Adventure/Assets/Scripts/StartMenuController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StartMenuController : MonoBehaviour
+{
+    public void StartClicked()
+    {
+        Debug.Log("Start clicked!");
+    }
+
+    public void LeaderboardClicked()
+    {
+        Debug.Log("Leaderboard clicked!");
+    }
+
+    public void ExitClicked()
+    {
+        Debug.Log("Exit clicked!");
+    }
+}
