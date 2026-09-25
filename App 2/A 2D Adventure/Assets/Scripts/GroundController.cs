c4fae73aba4fa4ae971a5106190c4a4bbb9135c1
Boris Vasilev <44681924+borisonekenobi@users.noreply.github.com>
2026-09-24 20:09:31 -0400

    Add GroundController script and update scene files for Level0_4 and Level1_0

---
+++ App 2/A 2D Adventure/Assets/Scripts/GroundController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GroundController : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        SceneChanger.Instance.MoveToScene(SceneManager.GetActiveScene().name);
+    }
+}
